24ad77b02d9f333ba8e752177ba67894d30a82b6
Reaper3825 <austintylerwhite@icloud.com>
2025-08-23 15:04:23 -0400

    Fix variable type and update output in main.cpp; change background color for consistency

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int a = 6;
+string a = Startup;
 
 int main() {
-    std::cout << "Value of a: " << a << std::endl;
+    std::cout << a << std::endl;
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Blank Window");
     window.setPosition({100,100});
@@ -19,7 +19,7 @@ int main() {
         // comment movement out for now to avoid it zipping off-screen
         // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) circle.move(2.f, 0.f);
 
-        window.clear(sf::Color(127, 30, 30));   // Background color
+        window.clear(sf::Color(225, 225, 225));   // Background color
 
         window.display();
     }
