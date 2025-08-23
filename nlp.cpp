95758357323cb350e8c0cab0a0461f780fd4939e
Reaper3825 <austintylerwhite@icloud.com>
2025-08-23 16:59:52 -0400

    --corrected nl.cpp

--- nlp.cpp
+++ nlp.cpp
@@ -1,31 +1,38 @@
+// nlp.cpp
 #include "nlp.hpp"
-#include "commands.hpp" // so we can call handleCommand
+#include "commands.hpp"
 #include <algorithm>
 #include <sstream>
 
 std::string parseNaturalLanguage(const std::string& line, fs::path& currentDir) {
+    // Lowercase for easier matching
     std::string lowered = line;
     std::transform(lowered.begin(), lowered.end(), lowered.begin(), ::tolower);
 
+    // "where am i" -> pwd
     if (lowered.find("where am i") != std::string::npos ||
         lowered.find("current directory") != std::string::npos) {
         return handleCommand("pwd", currentDir);
     }
 
+    // "list files" / "show files" -> list
     if (lowered.find("list files") != std::string::npos ||
         lowered.find("show files") != std::string::npos) {
         return handleCommand("list", currentDir);
     }
 
+    // "make folder" / "create folder" -> mkdir
     if (lowered.find("make folder") != std::string::npos ||
         lowered.find("create folder") != std::string::npos) {
         return handleCommand("mkdir new_folder", currentDir);
     }
 
+    // "delete old files" -> rmolder
     if (lowered.find("delete old") != std::string::npos) {
         return handleCommand("rmolder 30 -r", currentDir);
     }
 
+    // "move file.txt to backup" -> move file.txt backup
     if (lowered.find("move") != std::string::npos && lowered.find("to") != std::string::npos) {
         std::istringstream iss(lowered);
         std::string word, src, dst;
