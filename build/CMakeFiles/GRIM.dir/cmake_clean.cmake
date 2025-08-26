6725955d6e953b347201c946368e7af75b8c7a6d
Reaper3825 <austintylerwhite@icloud.com>
2025-08-26 14:34:30 -0400

    --built with cmake

---
+++ build/CMakeFiles/GRIM.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GRIM.dir/ai.cpp.obj"
+  "CMakeFiles/GRIM.dir/ai.cpp.obj.d"
+  "CMakeFiles/GRIM.dir/commands.cpp.obj"
+  "CMakeFiles/GRIM.dir/commands.cpp.obj.d"
+  "CMakeFiles/GRIM.dir/main.cpp.obj"
+  "CMakeFiles/GRIM.dir/main.cpp.obj.d"
+  "CMakeFiles/GRIM.dir/nlp.cpp.obj"
+  "CMakeFiles/GRIM.dir/nlp.cpp.obj.d"
+  "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj"
+  "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj.d"
+  "GRIM.exe"
+  "GRIM.exe.manifest"
+  "GRIM.pdb"
+  "libGRIM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GRIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
