61f34ebd9b93839cd0a83d5f8e309d36ffc4973d
Reaper3825 <austintylerwhite@icloud.com>
2025-08-26 14:34:30 -0400

    --built with cmake

---
+++ build/CMakeFiles/GRIM.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/GRIM Local/G.R.I.M/ai.cpp" "CMakeFiles/GRIM.dir/ai.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/ai.cpp.obj.d"
+  "D:/GRIM Local/G.R.I.M/commands.cpp" "CMakeFiles/GRIM.dir/commands.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/commands.cpp.obj.d"
+  "D:/GRIM Local/G.R.I.M/main.cpp" "CMakeFiles/GRIM.dir/main.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/main.cpp.obj.d"
+  "D:/GRIM Local/G.R.I.M/nlp.cpp" "CMakeFiles/GRIM.dir/nlp.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/nlp.cpp.obj.d"
+  "D:/GRIM Local/G.R.I.M/nlp_rules.cpp" "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
