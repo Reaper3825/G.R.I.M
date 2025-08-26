41af05423d1e19adb87124160ea03533192491f0
Reaper3825 <austintylerwhite@icloud.com>
2025-08-26 13:36:41 -0400

    --rebuilt

--- nlp.hpp
+++ nlp.hpp
@@ -3,3 +3,4 @@
 
 bool loadNlpRules(const std::string& path);   // <- no noexcept to avoid mismatch
 std::string mapTextToCommand(const std::string& lowered);
+std::string parseNaturalLanguage(const std::string& line, fs::path& currentDir);
