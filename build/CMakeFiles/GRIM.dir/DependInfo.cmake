86840bdccdfb9283a3d04b5681a09bab3375cb02
Reaper3825 <166045987+Reaper3825@users.noreply.github.com>
2025-08-28 17:57:27 -0400

    --added cross platform to Mac

--- build/CMakeFiles/GRIM.dir/DependInfo.cmake
+++ build/CMakeFiles/GRIM.dir/DependInfo.cmake
@@ -8,11 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "D:/GRIM Local/G.R.I.M/ai.cpp" "CMakeFiles/GRIM.dir/ai.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/ai.cpp.obj.d"
-  "D:/GRIM Local/G.R.I.M/commands.cpp" "CMakeFiles/GRIM.dir/commands.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/commands.cpp.obj.d"
-  "D:/GRIM Local/G.R.I.M/main.cpp" "CMakeFiles/GRIM.dir/main.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/main.cpp.obj.d"
-  "D:/GRIM Local/G.R.I.M/nlp.cpp" "CMakeFiles/GRIM.dir/nlp.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/nlp.cpp.obj.d"
-  "D:/GRIM Local/G.R.I.M/nlp_rules.cpp" "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj" "gcc" "CMakeFiles/GRIM.dir/nlp_rules.cpp.obj.d"
+  "/Users/austinwadkins/Documents/G.R.I.M/ai.cpp" "CMakeFiles/GRIM.dir/ai.cpp.o" "gcc" "CMakeFiles/GRIM.dir/ai.cpp.o.d"
+  "/Users/austinwadkins/Documents/G.R.I.M/commands.cpp" "CMakeFiles/GRIM.dir/commands.cpp.o" "gcc" "CMakeFiles/GRIM.dir/commands.cpp.o.d"
+  "/Users/austinwadkins/Documents/G.R.I.M/main.cpp" "CMakeFiles/GRIM.dir/main.cpp.o" "gcc" "CMakeFiles/GRIM.dir/main.cpp.o.d"
+  "/Users/austinwadkins/Documents/G.R.I.M/nlp.cpp" "CMakeFiles/GRIM.dir/nlp.cpp.o" "gcc" "CMakeFiles/GRIM.dir/nlp.cpp.o.d"
+  "/Users/austinwadkins/Documents/G.R.I.M/nlp_rules.cpp" "CMakeFiles/GRIM.dir/nlp_rules.cpp.o" "gcc" "CMakeFiles/GRIM.dir/nlp_rules.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/GRIM Local/G.R.I.M
+# Install script for directory: /Users/austinwadkins/Documents/G.R.I.M
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GRIM")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/GRIM Local/G.R.I.M/build/install_local_manifest.txt"
+  file(WRITE "/Users/austinwadkins/Documents/G.R.I.M/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/GRIM Local/G.R.I.M/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/austinwadkins/Documents/G.R.I.M/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
