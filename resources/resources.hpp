21d201cac9ecbe80bdef161b20fd86700a036b30
Reaper3825 <austintylerwhite@icloud.com>
2025-09-10 15:29:39 -0400

    --corrected typo

--- resources/resources.hpp
+++ resources/resources.hpp
@@ -1,8 +1,8 @@
-#pragma message("Using resources.hpp from project root")7
 #pragma once
 #include <string>
 #include <SFML/Graphics.hpp>
 #include "console_history.hpp"
+#pragma message("Using resources.hpp from project root")
 
 std::string getResourcePath();
 std::string loadTextResource(const std::string& filename, int argc, char** argv);
