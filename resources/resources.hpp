984f1bc51ffa84e9d78a9009ae260ac3c6985143
Reaper3825 <austintylerwhite@icloud.com>
2025-09-10 15:34:10 -0400

    --debugging resources.hpp

--- resources/resources.hpp
+++ resources/resources.hpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 #include "console_history.hpp"
 #pragma message("Using resources.hpp from project root")
+#error "THIS IS THE CORRECT resources.hpp"
+
 
 std::string getResourcePath();
 std::string loadTextResource(const std::string& filename, int argc, char** argv);
